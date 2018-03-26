ee1332e7de20896cf696ee9e29bc8330793a8fb3
gitogitton <mukyu_02@yahoo.co.jp>
2018-03-26 12:44:54 +0900

    「もっと見ます？」のリスナー登録処理追加

--- app/src/main/java/rokuroku/jp/rokutenappli/MainActivity.java
+++ app/src/main/java/rokuroku/jp/rokutenappli/MainActivity.java
@@ -24,60 +24,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        setViewPager();
     }
 
     private void showNekoShokai() {
         //うちの猫紹介fragmentでアピール
     }
 
-    private void setViewPager() {
-        ViewPager viewPager = findViewById( R.id.viewPager );
-        //set adapter for viewPager
-        MyViewPagerAdapter myPagerAdapter = new MyViewPagerAdapter( this );
-        viewPager.setAdapter( myPagerAdapter );
-        //set adapter of viewPager for SlidingTabStrip
-        PagerSlidingTabStrip tabs = findViewById( R.id.tabs );
-        tabs.setViewPager( viewPager );
-        //get tab resource
-        int tabCount = tabs.getTabCount();
-        LinearLayout linearLayout = tabs.getTabsContainer();
-        for ( int i=0; i<tabCount; i++ ) {
-            TextView textView = (TextView)linearLayout.getChildAt( i );
-            if ( i==0 ) {
-                textView.setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_red, 0, 0, 0 );
-            }
-            else {
-                textView.setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_blue, 0, 0, 0 );
-            }
-            mArrayList.add( textView );
-        }
-        //set listener
-        tabs.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//                Log.d( TAG, "TAB change Page. position / offset / offsetPixels->" + position + " / " + positionOffset + " / " + positionOffsetPixels );
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d( TAG, "onPageSelected() : Select Position->" + position + " Prev.->" + mPagerPos );
-                mArrayList.get( position ).setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_red, 0, 0, 0 );
-                mArrayList.get( mPagerPos ).setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_blue, 0, 0, 0 );
-                mPagerPos = position;
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-//                Log.d( TAG, "TAB Page scroll status. state->" + state );
-            }
-        });
-    }
-
     private void init() {
-        //猫紹介のLinearLayoutのリスナー登録
         //View取得
+        //猫紹介
         RelativeLayout relaiveNekoShokai = findViewById(R.id.main_head);
         //体重グラフ｜体脂肪グラフ　エリア
         LinearLayout linearGraphTaiju = findViewById(R.id.main_body02);
@@ -85,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         TextView textTaisiboGraph = linearGraphTaiju.findViewById(R.id.text_taisiboritu); //体脂肪グラフ
         //体脂肪変換ボタン
         LinearLayout linearHenkanShibo = findViewById(R.id.main_body03);
+        //「もっと見ます？」ボタン
+        RelativeLayout relativeMotto = findViewById( R.id.main_body05 );
 
         //リスナー登録
         relaiveNekoShokai.setOnClickListener(new View.OnClickListener() {
@@ -113,5 +70,57 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "linearHenkanShibo click Neko Shokai !");
             }
         });
+        relativeMotto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "relativeMotto click Motto !");
+            }
+        });
+
+        //ViewPgerの設定
+        setViewPager();
+
     }//init()
+
+    private void setViewPager() {
+        ViewPager viewPager = findViewById( R.id.viewPager );
+        //set adapter for viewPager
+        MyViewPagerAdapter myPagerAdapter = new MyViewPagerAdapter( this );
+        viewPager.setAdapter( myPagerAdapter );
+        //set adapter of viewPager for SlidingTabStrip
+        PagerSlidingTabStrip tabs = findViewById( R.id.tabs );
+        tabs.setViewPager( viewPager );
+        //get tab resource
+        int tabCount = tabs.getTabCount();
+        LinearLayout linearLayout = tabs.getTabsContainer();
+        for ( int i=0; i<tabCount; i++ ) {
+            TextView textView = (TextView)linearLayout.getChildAt( i );
+            if ( i==0 ) {
+                textView.setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_red, 0, 0, 0 );
+            }
+            else {
+                textView.setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_blue, 0, 0, 0 );
+            }
+            mArrayList.add( textView );
+        }
+        //set listener
+        tabs.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                Log.d( TAG, "TAB change Page. position / offset / offsetPixels->" + position + " / " + positionOffset + " / " + positionOffsetPixels );
+            }
+            @Override
+            public void onPageSelected(int position) {
+                Log.d( TAG, "onPageSelected() : Select Position->" + position + " Prev.->" + mPagerPos );
+                mArrayList.get( position ).setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_red, 0, 0, 0 );
+                mArrayList.get( mPagerPos ).setCompoundDrawablesRelativeWithIntrinsicBounds( R.drawable.icon_blue, 0, 0, 0 );
+                mPagerPos = position;
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+//                Log.d( TAG, "TAB Page scroll status. state->" + state );
+            }
+        });
+    }
+
 }
